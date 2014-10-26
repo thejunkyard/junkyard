3535572ecbed27dc93830c20fd6acbf9d06c3dba
slifty <slifty@gmail.com>
2014-10-26 18:19:58 +0000

    working first version

--- junkyard/public/javascripts/stream.js
+++ junkyard/public/javascripts/stream.js
@@ -1,17 +1,24 @@
 $(function() {
   var $stream = $("#stream");
 
-  $.ajax({
-    method: "get",
-    url: "api/posts"
-  })
-  .success(function(data){
-    var posts = data.posts;
-    for(var x in data.posts) {
-      var post = data.posts.x;
-      var $post = $("<li>");
-      $post.text(post.content)
-      $post.appendTo($stream);
-    }
-  })
+  function refresh() {
+    $stream.empty();
+    $.ajax({
+      method: "get",
+      url: "api/posts"
+    })
+    .success(function(data){
+      var posts = data.posts;
+      for(var x in data.posts) {
+        var post = data.posts[x];
+        var $post = $("<li>");
+        $post.text(post.content);
+        $post.appendTo($stream);
+      }
+    })
+
+    setTimeout(refresh, 5000);
+  }
+
+  refresh();
 })
--- junkyard/routes/api_posts.js
+++ junkyard/routes/api_posts.js
@@ -7,29 +7,36 @@ var Post = require('../models/post');
 // Return a list of the most recent posts
 router.get('/',function(req, res) {
   Post.find().sort('-date').limit(10).exec(function(err, posts) {
-    res.json(posts);
+    res.json({posts: posts});
+  });
+});
+
+// Return a random post
+router.get('/random',function(req, res) {
+  Post.find().sort('date').limit(1).exec(function(err, post) {
+    res.json({ post: post[0] });
   });
 });
 
 // Return a specific post
 router.get('/:id',function(req, res) {
   Post.find({id:req.query.id}).exec(function(err, post) {
-    res.json(post);
+    res.json({ post: post[0] });
   });
-});
-
-// Return a random post
-router.get('/random',function(req, res) {
-  res.json({message:'yay'})
 });
 
 // Create a new post
 router.post('/',function(req, res) {
   var post = new Post({
     name: req.body.name,
-    owner_id: req.body.owner_id,
-    response_to: req.body.content,
     content: req.body.content
+  });
+
+  post.save(function(err) {
+    res.json({
+      cooldown: 3 * 60 * 1000, // 3m cooldown
+      post: post
+    })
   });
 });
 
